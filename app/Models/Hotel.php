45cfd77d9ea07e9808b592079fdda5f40243c035
songitcode <dung22022017cm@gmail.com>
2024-10-19 20:13:09 +0700

    add-code-model-hotel

--- app/Models/Hotel.php
+++ app/Models/Hotel.php
@@ -9,6 +9,14 @@ class Hotel extends Model
 {
     use HasFactory;
 
-     // Chỉ định tên bảng nếu nó khác với quy tắc mặc định
+    // Chỉ định tên bảng nếu nó khác với quy tắc mặc định
     //  protected $table = 'your_custom_table_name';
+    protected $table = 'hotels'; // Tên bảng
+    protected $primaryKey = 'hotel_id'; // Khóa chính
+
+    // Nếu không có timestamps trong bảng:
+    public $timestamps = false;
+
+    // Khai báo các trường được phép fill
+    protected $fillable = ['hotel_name', 'location', 'city_id', 'description', 'rating'];
 }
