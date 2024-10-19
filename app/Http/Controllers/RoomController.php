bf3d8312e63e9f4c37074cd7784249af6f43bbf4
Vo Van So <vovanso2004@gmail.com>
2024-10-19 23:36:21 +0700

    Complete ViewRoom

--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -17,7 +17,26 @@ class RoomController extends Controller
         $rooms = Rooms::getAllRooms();
         return view('admin.room', ['rooms' => $rooms]);
     }
+    public function show(Request $request)
+    {
+        // Lấy danh sách loại phòng và tiện nghi
+        $roomTypes = RoomType::all();
+        $amenities = RoomAmenities::all();
 
+        // Trả về view với dữ liệu
+        return view('admin.room_add', [
+            'roomTypes' => $roomTypes,
+            'amenities' => $amenities,
+        ]);
+    }
+    public function destroy($room_id)
+    {
+        // Tìm phòng dựa trên ID
+        if (Rooms::deleteRoom($room_id)) {
+            return redirect()->route('admin.viewroom')->with('success', 'Phòng đã được xóa thành công!');
+        }
+        return redirect()->route('admin.viewroom')->with('error', 'Phòng không tồn tại!');
+    }
    
 
 
