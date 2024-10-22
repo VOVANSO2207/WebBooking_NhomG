8d40733737f911d36735449ecfe06035e58a5c6b
Vo Van So <vovanso2004@gmail.com>
2024-10-22 07:23:29 +0700

    checkout branch dev

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ use App\Http\Controllers\UsersController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\HotelController;
 use App\Http\Controllers\CitiesController;
-
+    
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
