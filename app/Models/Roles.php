9a8c0344ca8e2de5e018c92a12f3a850235f5d0b
CaoTranMinhTam0811 <22211tt2962@mail.tdc.edu.vn>
2024-10-24 23:56:28 +0700

    update roles model

--- app/Models/Roles.php
+++ app/Models/Roles.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Roles extends Model
 {
     use HasFactory;
+
+    protected $table = 'roles'; // Tên bảng
+    protected $primaryKey = 'role_id'; // Đặt khóa chính
+    public $incrementing = true; // Nếu role_id tự động tăng
+    protected $keyType = 'int'; // Kiểu dữ liệu của khóa chính
+    protected $fillable = ['role_name']; // Thuộc tính có thể được gán hàng loạt
 }
