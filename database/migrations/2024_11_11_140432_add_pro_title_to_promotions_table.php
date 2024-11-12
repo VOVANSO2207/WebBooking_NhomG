430b5b8219b8862df2c66704cfc71ad927a67bf4
Thanh Lich <tlich077@gmail.com>
2024-11-12 11:51:53 +0700

    commit

--- database/migrations/2024_11_11_140432_add_pro_title_to_promotions_table.php
+++ database/migrations/2024_11_11_140432_add_pro_title_to_promotions_table.php
@@ -4,23 +4,22 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up()
-{
-    Schema::table('promotions', function (Blueprint $table) {
-        $table->string('pro_title')->nullable(); // Thêm trường pro_title
-    });
-}
+    {
+        Schema::table('promotions', function (Blueprint $table) {
+            $table->string('pro_title')->nullable(); // Thêm trường pro_title
+        });
+    }
 
-public function down()
-{
-    Schema::table('promotions', function (Blueprint $table) {
-        $table->dropColumn('pro_title'); // Xóa trường pro_title khi rollback
-    });
-}
+    public function down()
+    {
+        Schema::table('promotions', function (Blueprint $table) {
+            $table->dropColumn('pro_title'); // Xóa trường pro_title khi rollback
+        });
+    }
 
 };
